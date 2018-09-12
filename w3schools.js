14ffcf6800c2ad2cadd08cccb7d2301414447f5c
toussyn <43005376+toussyn@users.noreply.github.com>
2018-09-12 07:46:26 +0100

    Update w3schools.js

--- w3schools.js
+++ w3schools.js
@@ -1,5 +1,5 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function myFunction1() {
+    var x = document.getElementById("myDIV1");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
